9f42914ac89c55287e2d8470bdce4f6991cbf568
Om Avhad <odavhad@gmail.com>
2021-07-12 14:40:14 +0530

    Create database.py

---
+++ shrty/database.py
@@ -0,0 +1,23 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL").replace(
+    "postgres://", "postgresql://", 1
+)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = session()
+
+    try:
+        yield db
+
+    finally:
+        db.close()
