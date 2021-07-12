5197b9c71a3cc2616fcd9d31da27a9c5ffcba3a0
Om Avhad <odavhad@gmail.com>
2021-07-12 16:13:47 +0530

    Update schemas.py

--- shrty/schemas.py
+++ shrty/schemas.py
@@ -1,6 +1,29 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class UserAuth(BaseModel):
     username: str
     password: str
+
+
+class URLSchema(BaseModel):
+    short_tag: str
+    target_url: str
+    public: Optional[bool] = False
+
+
+class URLSchemaCreate(URLSchema):
+    visit_count: Optional[int] = 0
+
+
+class URLSchemaModify(URLSchema):
+    visit_count: Optional[int]
+
+
+class URLDatabase(URLSchema):
+    id: int
+
+    class Config:
+        orm_mode = True
+
