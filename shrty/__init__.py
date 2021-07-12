7978855500aa7d38991d5e42f69b651bafcea101
Om Avhad <odavhad@gmail.com>
2021-07-12 14:30:43 +0530

    Create authorize user route function

--- shrty/__init__.py
+++ shrty/__init__.py
@@ -1,11 +1,28 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, HTTPException, status
+
+from shrty import auth, schemas
 
 
 app = FastAPI()
 
+auth_handler = auth.AuthHandler()
 
-@app.get('/')
-def hello():
-    return {
-        "detail": "hello!"
-    }
+
+@app.post("/auth", tags=["auth"])
+def authorize_user(user: schemas.UserAuth):
+    USERNAME = os.getenv("USER_NAME")
+
+    if user.username != USERNAME:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"description": "invalid username."},
+        )
+
+    elif not auth_handler.verify(user.password):
+        raise HTTPException(
+            status_code=status.HTTP_406_NOT_ACCEPTABLE,
+            detail={"description": "invalid password."},
+        )
+
+    return auth_handler.encode_token(user.username)
