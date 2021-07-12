cebab1d9f5007eb0de88f39f01398368e913f098
Om Avhad <odavhad@gmail.com>
2021-07-12 14:44:39 +0530

    Deployment test

--- shrty/__init__.py
+++ shrty/__init__.py
@@ -1,11 +1,22 @@
 import os
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import RedirectResponse
+from sqlalchemy.orm import Session
 
-from shrty import auth, schemas
+from shrty import auth, models, router, schemas
+from shrty.database import engine, get_db
+
+
+models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
+
+templates = Jinja2Templates(directory="app/templates")
 auth_handler = auth.AuthHandler()
 
 
@@ -26,3 +37,56 @@ def authorize_user(user: schemas.UserAuth):
         )
 
     return auth_handler.encode_token(user.username)
+
+
+@app.get("/")
+def get_all_urls(request: Request, db: Session = Depends(get_db)):
+    urls = (
+        db.query(models.URLModel.short_tag, models.URLModel.target_url)
+        .filter(models.URLModel.public == True)
+        .order_by(models.URLModel.short_tag)
+        .all()
+    )
+
+    return templates.TemplateResponse("index.html", {"request": request, "urls": urls})
+
+
+@app.get("/stats", tags=["auth"])
+def get_url_stats(
+    db: Session = Depends(get_db), user: str = Depends(auth_handler.auth_wrapper)
+):
+    urls = db.query(models.URLModel).order_by(models.URLModel.short_tag).all()
+
+    return urls
+
+
+app.include_router(router.crud_router)
+
+
+@app.get("/404")
+def not_found(request: Request):
+    return templates.TemplateResponse("404.html", {"request": request})
+
+
+@app.get("/{short_tag}")
+def route(short_tag: str, db: Session = Depends(get_db)):
+    db_url = (
+        db.query(models.URLModel).filter(models.URLModel.short_tag == short_tag).first()
+    )
+
+    print(db_url)
+
+    if db_url:
+        response = RedirectResponse(db_url.target_url)
+
+        db.query(models.URLModel).filter(models.URLModel.short_tag == short_tag).update(
+            {"visit_count": db_url.visit_count + 1}
+        )
+        db.commit()
+
+        return response
+
+    else:
+        response = RedirectResponse("404")
+
+        return response
