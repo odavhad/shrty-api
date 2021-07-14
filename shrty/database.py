4ae179cd813d57002ba491be81bc51a3de4518bd
Om Avhad <odavhad@gmail.com>
2021-07-14 16:30:01 +0530

    Minor fixes

--- shrty/database.py
+++ shrty/database.py
@@ -3,9 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL").replace(
-    "postgres://", "postgresql://", 1
-)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
